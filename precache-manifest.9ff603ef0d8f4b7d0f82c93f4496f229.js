505a848e577806cc5267d814358dfaf7bf5d2cb5
Higuinen <mhiguinen235@gmail.com>
2021-05-29 14:01:51 +0200

    Updates

---
+++ precache-manifest.9ff603ef0d8f4b7d0f82c93f4496f229.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affe5e411bb4497d7054325d210b5a84",
+    "url": "/SiteReact/index.html"
+  },
+  {
+    "revision": "7fb0a15ae129ea6478bb",
+    "url": "/SiteReact/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "ea338a869db6ed2e7f4a",
+    "url": "/SiteReact/static/js/2.efd6651a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SiteReact/static/js/2.efd6651a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb0a15ae129ea6478bb",
+    "url": "/SiteReact/static/js/main.2d45c530.chunk.js"
+  },
+  {
+    "revision": "b9bebd3d699a4451f47a",
+    "url": "/SiteReact/static/js/runtime-main.fb1cd044.js"
+  },
+  {
+    "revision": "b65a7b3c5835ccc1450f16390f10b25c",
+    "url": "/SiteReact/static/media/svg-1.b65a7b3c.svg"
+  },
+  {
+    "revision": "2cb77f210e102a16150846821d873212",
+    "url": "/SiteReact/static/media/svg-2.2cb77f21.svg"
+  },
+  {
+    "revision": "fb0424e13af0c136c0a7e2d3b0072e1f",
+    "url": "/SiteReact/static/media/svg-3.fb0424e1.svg"
+  },
+  {
+    "revision": "37aab918c395989c92a143556e7d3f5f",
+    "url": "/SiteReact/static/media/svg-7.37aab918.svg"
+  },
+  {
+    "revision": "1812106de41f61a44091223d5cecfebc",
+    "url": "/SiteReact/static/media/svg-8.1812106d.svg"
+  },
+  {
+    "revision": "3cb2fa2c0a0b936a1d23bb3bf139a120",
+    "url": "/SiteReact/static/media/video.3cb2fa2c.mp4"
+  }
+]);
